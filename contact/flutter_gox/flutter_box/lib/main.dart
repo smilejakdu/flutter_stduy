281e6f9516680c8cb94accbbd44861365279746e
작두 <ash982416@gmail.com>
2022-02-07 02:25:48 +0900

    box flutter study

---
+++ contact/flutter_gox/flutter_box/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(title: Text('앱임')),
+      body: Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          width: double.infinity,
+          height: 50,
+          color: Colors.blue,
+        ),
+      ),
+    ));
+  }
+}
